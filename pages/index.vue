3bd44f54b03142eae882e1eb90ab4a6aee20b1f9
yaoshihui <809281246@qq.com>
2019-12-16 13:06:10 +0800

    ..

---
+++ pages/index.vue
@@ -0,0 +1,178 @@
+<template>
+	<view class="index">
+		<!-- 轮播 -->
+		<view class="page-section-spacing banner">
+			<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item  v-for="(item,index) in bannerList" :key="index">
+					<image class="bannerImg" :src="item.img" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<!-- 导航 -->
+		<view class="nav">
+			<block v-for="(item,index) in nav" :key="index">
+				<view class="navList" @tap="goTo" :data-id="item.id">
+					<image :src="item.img" mode="aspectFill"></image>
+					<text>{{item.title}}</text>
+				</view>
+			</block>
+		</view>
+		<!-- 数据列表 -->
+		<view class="content">
+			 <view class="inv-h-w">
+				<view :class="['inv-h',Inv==0?'inv-h-se':'']" @tap="Inv=0">最新产品解析</view>
+				<view :class="['inv-h',Inv==1?'inv-h-se':'']" @tap="Inv=1">办卡提额技术</view>
+			</view>
+			<view class="contentList">
+				<view class="item">
+					<image src="../static/a.jpg" mode=""></image>
+					<view class="itemRight">
+						<text>云南城投股吧说说股票风险如何控制</text>
+						<view>
+							<text>1</text>
+							<text>2</text>
+							<text>3</text>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: 'Hello',
+			background: ['color1', 'color2', 'color3'],
+			indicatorDots: true,
+			autoplay: true,
+			interval: 2000,
+			duration: 500,
+			bannerList:[
+				{id:'1',img:'../static/a.jpg'},
+				{id:'2',img:'../static/b.jpg'},
+				{id:'12',img:'../static/a.jpg'},
+			],
+			nav:[
+				{id:'1',title:'产品超市',img:'../static/a.jpg'},
+				{id:'2',title:'信贷技术',img:'../static/a.jpg'},
+				{id:'3',title:'办卡提额',img:'../static/a.jpg'},
+				{id:'1',title:'信用修复',img:'../static/a.jpg'},
+				{id:'1',title:'查询工具',img:'../static/a.jpg'},
+				{id:'1',title:'精准匹配',img:'../static/a.jpg'},
+				{id:'1',title:'最新资讯',img:'../static/a.jpg'},
+				{id:'1',title:'小微企业',img:'../static/a.jpg'},
+				{id:'1',title:'负债重组',img:'../static/a.jpg'},
+				{id:'1',title:'开通会员',img:'../static/a.jpg'}
+			],
+			Inv:0
+		};
+	},
+	onLoad() {
+		console.log(this.title);
+	},
+	methods: {
+		// 导航详情
+		goTo(e){
+			console.log(e)
+		},
+		//
+		changeTab(Inv){
+			that.navIdx = Inv;
+		},
+	}
+};
+</script>
+
+<style>
+.index {
+	width: 750rppx;
+}
+.banner{
+	width: 690rpx;
+	margin: 30rpx;
+	background: #007AFF;
+	height: 300rpx;
+	border-radius: 10rpx;
+	overflow: hidden;
+}
+.bannerImg{
+	width: 690rpx;
+	height: 300rpx;
+}
+.nav{
+	width: 690rpx;
+	padding: 30rpx;
+	/* background: #007AFF; */
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+	/* border-bottom:12rpx solid #f9f9f9; */
+	border-bottom:12rpx solid #ccc;
+}
+.nav .navList{
+	width:116rpx;
+	margin-right: 27.99rpx;
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	margin-bottom: 38rpx;
+}
+.nav .navList:nth-child(5n){
+	margin-right: 0;
+}
+.nav .navList text{
+	font-size: 28rpx;
+	color: #333333;
+	font-weight: 600;
+	display: block;
+}
+.nav .navList image{
+	width: 50rpx;
+	height: 50rpx;
+	margin-bottom: 14rpx;
+}
+.content{
+	width: 690rpx;
+	padding: 0 30rpx;
+}
+.content .inv-h-w{
+	display: flex;
+}
+.content .inv-h{
+	font-size: 32rpx;
+	flex: 1;
+	text-align: center;
+	color: #999999;
+	padding: 30rpx 0;
+}
+.content .inv-h-se{
+	color: #2390DC;
+	font-weight: 600;
+}
+.content .inv-h-se:after{
+	content: " ";
+	display: block;
+	border-bottom: 6rpx solid #2390DC;
+	width: 46rpx;
+	margin: 26rpx auto 0;
+	border-radius: 3rpx;
+}
+.content .contentList .item{
+	display: flex;
+	justify-content: space-between;
+}
+.content .contentList .item image{
+	width: 220rpx;
+	height: 136rpx;
+	border-radius: 10rpx;
+}
+.content .contentList .item .itemRight{
+	width: 440rpx;
+	display: flex;
+	align-content: space-between;
+	flex-wrap: wrap;
+}
+</style>
---
+++ pages/registered.vue
@@ -0,0 +1,99 @@
+<template>
+	<view class="login">
+		<view class="item">
+			<input type="text" placeholder="请输入用户名/手机号" @input="getLoginName" />
+		</view>
+		<view class="item">
+			<input type="password" placeholder="请输入密码" @input="getLoginPaw" />
+			<view class="forget">
+				<text @tap="forgetPassword">忘记密码</text>
+			</view>
+		</view>
+		<view class="item">
+			<view class="submit" @tap="login">登录</view>
+		</view>
+		<view class="item">
+			<view class="registered" @tap="registered">注册</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			console.log(this.title)
+		},
+		methods: {
+			getLoginName(e) {
+				this.loginName = e.detail.value
+				console.log(this.loginName)
+				
+			},
+			getLoginPaw(e) {
+				this.loginPaw = e.detail.value
+				console.log(this.loginPaw)
+			}
+		}
+	}
+</script>
+
+<style>
+	.login {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin-top: 100rpx;
+	}
+	.item{
+		width: 670rpx;
+		height: auto;
+		padding: 40rpx;
+	}
+	input{
+		width: 630rpx;
+		height: 80rpx;
+		border-radius: 10rpx;
+		background-color: #F8F8F8;
+		color: #333333;
+		font-size: 30rpx;
+		padding: 0 20rpx;
+	}
+	.forget{
+		display: flex;
+		align-items: center;
+		justify-content: flex-end;
+		font-size: 23rpx;
+		color: #999999;
+		padding-top: 20rpx;
+	}
+	.submit{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 670rpx;
+		height: 80rpx;
+		border-radius: 10rpx;
+		background-color: #2390DC;
+		color: #ffffff;
+		font-size: 30rpx;
+		font-weight: 600;
+	}
+	.registered{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 670rpx;
+		height: 80rpx;
+		border-radius: 10rpx;
+		background-color: #ffffff;
+		color: #666666;
+		font-size: 30rpx;
+		font-weight: 600;
+	}
+</style>
