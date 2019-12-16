5b98c372764408c2a199d450ae7a2200e06c7e2a
yaoshihui <809281246@qq.com>
2019-12-16 16:02:28 +0800

    +++

---
+++ pages/apply.vue
@@ -0,0 +1,110 @@
+<template>
+	<view class="apply">
+		<pageSearch></pageSearch>
+		<view class="collection">
+			<text class="title">干货汇总</text>
+			<view class="collectionList">
+				<block  v-for="(item,index) in collectionList" :key="index">
+					<view class="item">
+						<image class="img" :src="item.img" mode=""></image>
+						<text>{{item.title}}</text>
+					</view>
+				</block>
+			</view>
+		</view>
+		<view class="collection">
+			<text class="title">信用卡工具</text>
+			<view class="collectionList">
+				<block  v-for="(item,index) in collectionList" :key="index">
+					<view class="item">
+						<image class="img" :src="item.img" mode=""></image>
+						<text>{{item.title}}</text>
+					</view>
+				</block>
+			</view>
+		</view>
+		<view class="collection">
+			<text class="title">微金工具</text>
+			<view class="collectionList">
+				<block  v-for="(item,index) in collectionList" :key="index">
+					<view class="item">
+						<image class="img" :src="item.img" mode=""></image>
+						<text>{{item.title}}</text>
+					</view>
+				</block>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				collectionList:[
+					{id:'1',title:'123',img:'../static/a.jpg'},
+					{id:'1',title:'123',img:'../static/a.jpg'},
+					{id:'1',title:'123',img:'../static/a.jpg'},
+					{id:'1',title:'123',img:'../static/a.jpg'},
+					{id:'1',title:'123',img:'../static/a.jpg'},
+					{id:'1',title:'1征信查询23',img:'../static/a.jpg'},
+					{id:'1',title:'征信查询',img:'../static/a.jpg'},
+					{id:'1',title:'征信查询',img:'../static/a.jpg'},
+					{id:'1',title:'征信查询',img:'../static/a.jpg'},
+					{id:'1',title:'征信查询',img:'../static/a.jpg'}
+				]
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.apply{
+	width: 750rpx;
+}
+.collection{
+	width: 690rpx;
+	padding: 30rpx 30rpx 0;
+}
+.collection .title{
+	color: #333333;
+	font-size: 32rpx;
+	font-weight: 600;
+}
+.collectionList{
+	margin: 30rpx 0;
+	display: flex;
+	justify-content: flex-start;
+	flex-wrap: wrap;
+}
+.collectionList .item{
+	/* margin-right: ; */
+	width: 131rpx;
+	margin:0 8rpx 30rpx 0;
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+}
+.collectionList .item:nth-child(5n){
+	margin-right: 0;
+}
+.collectionList .item image{
+	width: 96rpx;
+	height: 96rpx;
+	border-radius: 50rpx;
+	display: block;
+}
+.collectionList .item text{
+	display: block;
+	font-size: 28rpx;
+	color: #333333;
+	font-weight: 600;
+	margin-top: 16rpx;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+}
+</style>
---
+++ pages/collection.vue
@@ -0,0 +1,108 @@
+<template>
+	<view class="collection">
+		<pageSearch></pageSearch>
+		<view class="page-section-spacing banner">
+			<swiper class="swiper" :indicator-dots="indicatorDots" indicator-color="rgba(255,255,255,.3)" indicator-active-color="#fff" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item  v-for="(item,index) in bannerList" :key="index">
+					<image class="bannerImg" :src="item.img" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="contentList">
+			<view class="item" v-for="(item,index) in list" :key="index">
+				<image :src="item.img" mode="aspectFill"></image>
+				<view class="itemRight">
+					<text class="title">{{item.title}}</text>
+					<view class="itemCon">
+						<text>{{item.time}}</text>
+						<text>{{item.name}}</text>
+						<text>{{item.num}}评</text>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				autoplay: true,
+				interval: 2000,
+				bannerList:[
+					{id:'1',img:'../static/a.jpg'},
+					{id:'2',img:'../static/b.jpg'},
+					{id:'12',img:'../static/a.jpg'},
+				],
+				list:[
+					{id:'1',img:'../static/b.jpg',time:'12小时前',name:'admin',num:'3',title:'云南城投股吧说说股票风险如何控制云南城投股吧说说股票风险如何控制云南城投股吧说说股票风险如何控制'},
+					{id:'1',img:'../static/b.jpg',time:'11小时前',name:'admin',num:'3',title:'dgfdhdyju'},
+					{id:'1',img:'../static/b.jpg',time:'12小时前',name:'admin',num:'2',title:'云南城投股吧说说股票风险如何控制云南城投股吧'}
+				]
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.collection{
+	width: 750rpx;
+}
+.banner{
+	width: 690rpx;
+	margin: 20rpx 30rpx;
+	border-radius: 10rpx;
+	overflow: hidden;
+}
+.bannerImg{
+	width: 690rpx;
+
+}
+.collection .contentList{
+	width: 690rpx;
+	padding: 10rpx 30rpx 30rpx;
+}
+.collection .contentList .item{
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 40rpx;
+}
+.collection .contentList .item image{
+	width: 220rpx;
+	height: 136rpx;
+	border-radius: 10rpx;
+}
+.collection .contentList .item .itemRight{
+	width: 440rpx;
+	display: flex;
+	align-content: space-between;
+	flex-wrap: wrap;
+}
+.collection .contentList .item .itemRight .title{
+	font-size: 28rpx;
+	color: #333333;
+	font-weight: 600;
+	overflow : hidden;
+	text-overflow: ellipsis;
+	display: -webkit-box;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
+}
+.collection .itemCon{
+	width: 440rpx;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	/* justify-content: flex-end; */
+	
+}
+.collection .itemCon text{
+	display: block;
+	font-size: 24rpx;
+	color: #999999;
+}
+</style>
