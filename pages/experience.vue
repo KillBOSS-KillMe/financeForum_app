fc50f031963f3114c39d6a25d60a1982366e99ad
yaoshihui <809281246@qq.com>
2019-12-17 15:10:57 +0800

    ++++++++++++

---
+++ pages/experience.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="experience">
+		<view class="item">
+			<image class="logo" src="../static/logo.png" mode=""></image>
+			<text>贷款口子交流</text>
+		</view>
+		<view class="item">
+			<image class="logo" src="../static/logo.png" mode=""></image>
+			<text>信用卡交流</text>
+		</view>
+		<view class="item">
+			<image class="logo" src="../static/logo.png" mode=""></image>
+			<text>会员帮助中心</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.experience{
+	width: 690rpx;
+	padding: 10rpx 30rpx;
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: wrap;
+}
+.item{
+	display: flex;
+	margin-bottom: 40rpx;
+	align-items: center;
+}
+
+.item .logo{
+	width: 58rpx;
+	height: 58rpx;
+	margin-right: 20rpx;
+}
+.item>text{
+	font-size: 30rpx;
+	font-weight: 600;
+	color: #333333;
+}
+</style>
---
+++ pages/post.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="post">
+		<input class="title" type="text" value="" placeholder="请输入标题" @input="inputValue">
+		<view class="content">
+			<textarea value="" placeholder="请输入发布内容..." />
+			<text class="tip">贷款进口子交流</text>
+		</view>
+		<view class="sound">
+			<uni-icon class="iconfont iconzanzan" type=""></uni-icon>
+			<text>附加语音</text>
+		</view>
+		<view class="sound">
+			<uni-icon class="iconfont iconzanzan" type=""></uni-icon>
+			<text class="address">显示所在位置</text>
+		</view>
+		<view class="enclosure">
+			<view class="enclosureList">
+				<uni-icon class="iconfont iconzanzan" type=""></uni-icon>
+				<uni-icon class="iconfont iconzanzan" type=""></uni-icon>
+				<uni-icon class="iconfont iconzanzan" type=""></uni-icon>
+				<uni-icon class="iconfont iconzanzan" type=""></uni-icon>
+			</view>
+		</view>
+		<button type="primary" class="submit">发帖</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.post{
+	width: 690rpx;
+	padding: 10rpx 30rpx;
+}
+.post .title{
+	border: 1rpx solid #F8F8F8;
+	padding: 0rpx 20rpx;
+	border-radius: 10rpx;
+	color: #999999;
+	font-size: 30rpx;
+	height: 80rpx;
+	line-height: 80rpx;
+}
+.post .content{
+	width: 630rpx;
+	height: 240rpx;
+	border: 1rpx solid #F8F8F8;
+	padding: 28rpx;
+	margin: 30rpx 0;
+	border-radius: 10rpx;
+}
+.post .content textarea{
+	height: 210rpx;
+	width: 636rpx;
+}
+.post .content .tip{
+	color: #999999;
+	font-size: 28rpx;
+	text-align: right;
+}
+.sound{
+	display: flex;	
+	width: 638rpx;
+	height: 28rpx;
+	border: 1rpx solid #F8F8F8;
+	border-radius: 10rpx;
+	align-items: center;
+	padding: 26rpx;
+	margin-bottom: 30rpx;
+}
+.sound text{
+	font-size: 30rpx;
+	font-weight: 600;
+	color: #333333;
+	margin-left: 16rpx;
+}
+.sound .address{
+	color: #999999;
+	font-weight: 200;
+}
+.enclosure{
+	padding:0 30rpx 100rpx;
+}
+.enclosureList>icon{
+	margin-right: 40rpx;
+}
+.submit{
+	width: 630rpx;
+	position: fixed;
+	bottom: 60rpx;
+	left: 60rpx;
+}
+</style>
